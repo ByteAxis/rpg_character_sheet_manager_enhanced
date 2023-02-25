a6f8bfbf6604a0d0175666a0b8e92627978e00e3
ByteAxis <86178026+ByteAxis@users.noreply.github.com>
2023-02-25 17:48:56 -0500

    Added spell removal to the magic-configuration.js page. Memory allocation is the next step followed by the loading onto the magic config page. Afterward, a magic interface class can be made for the player to select/search/sort spells to learn and prepare.

--- project-files/js/spell-functions.js
+++ project-files/js/spell-functions.js
@@ -21,3 +21,31 @@ function addSpell() {
             \
             ')
 }
+
+function removeSpell(buttonelement) {
+
+    var inputFields = document.getElementById("spell-container").children; // Store the list of spell forms
+    // 1. De-allocate deleted object's memory
+    for (var i = 0; i < inputFields.length; i++) {
+        if (inputFields[i] === buttonelement.parentNode) {
+            localStorage.removeItem("spellName" + (i + 1));
+            localStorage.removeItem("spellValue" + (i + 1));
+        }
+    }
+    buttonelement.parentNode.remove(); // Delete the visual component of the element
+
+    // 2. Shift all displaced elements to cover-down the gap in memory space.
+    for (var i = 0; i < inputFields.length; i++) {
+        if (localStorage.getItem("spellName" + (i + 1)) === null) { // When a gap in the new storage values is detected,
+            localStorage.setItem("spellName" + (i + 1), localStorage.getItem("spellName" + (i + 2))); // Copy the next item into the current gap, then delete the item's old memory spaces (both name and value)
+            localStorage.setItem("spellValue" + (i + 1), localStorage.getItem("spellValue" + (i + 2))); // Do the same to the spell value as well as the spell name.
+            localStorage.removeItem("spellName" + (i + 2));
+            localStorage.removeItem("spellValue" + (i + 2));
+            inputFields[i].querySelector("label").innerHTML = "Spell " + (i + 1) + ":";
+        }
+    }
+    // 3. De-allocated unused memory
+    var finalSpellIndex = (inputFields.length + 1); // Use this variable to prevent off-by-one error during memory de-allocation
+    localStorage.removeItem("spellName" + finalSpellIndex); // De-allocate unused memory
+    localStorage.removeItem("spellValue" + finalSpellIndex); // De-allocate unsued memory
+}
